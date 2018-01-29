404dffd0d8be519fac4b4e1a773d468d26497017
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-29 15:05:23 +0000

    initial setup with nwb

---
+++ src/FieldRenderProps.js
@@ -0,0 +1,27 @@
+// prop types based on https://github.com/final-form/react-final-form/blob/master/src/types.js.flow
+import PropTypes from "prop-types";
+
+const FieldRenderProps = {
+  input: PropTypes.shape({
+    name: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    value: PropTypes.any
+  }),
+  meta: PropTypes.shape({
+    active: PropTypes.bool,
+    dirty: PropTypes.bool,
+    dirtySinceLastSubmit: PropTypes.bool,
+    error: PropTypes.any,
+    initial: PropTypes.bool,
+    invalid: PropTypes.bool,
+    pristine: PropTypes.bool,
+    submitError: PropTypes.any,
+    submitFailed: PropTypes.bool,
+    submitSucceeded: PropTypes.bool,
+    touched: PropTypes.bool,
+    valid: PropTypes.bool,
+    visited: PropTypes.bool
+  })
+};
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { FieldRenderProps } from './FieldRenderProps'
